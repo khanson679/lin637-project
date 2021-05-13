81a4e4af9d939e04ae996be32b69ee95f620d50d
Kenneth Hanson <khanson679@gmail.com>
2021-05-13 17:00:45 -0400

    Correct transitions for VP and IP.

--- transducers.py
+++ transducers.py
@@ -17,23 +17,26 @@ trans = {
     (('qNbar',), 'NP'): ('qXP', Tree.from_list(['DP', 'D', 0])),
     (('qN', 'qXP'), "N'"): ('qNbar', Tree.from_list(['NP', 'N', 0])),
     (('qXP', 'qNbar'), "NP"): ('qXP', Tree.from_list(['DP', 0, 1])),
-    ((), 'V'): ('qV', Tree('V')),
-    (('qV',), "V'"): ('qVbar', Tree.from_list(["V'", 0])),
-    (('qVbar',), 'VP'): ('qXP', Tree.from_list(['VP', 0])),
-    (('qV', 'qXP'), "V'"): ('qVbar', Tree.from_list(["V'", 0, 1])),
-    (('qXP', 'qVbar'), "VP"): ('qXP', Tree.from_list(['VP', 0, 1])),
+    ((), 'V'): ('qV', Tree('')),
+    (('qV',), "V'"): ('qV', Tree('')),
+    (('qV',), 'VP'): ('qXP', Tree('VP')),
+    (('qV', 'qXP'), "V'"): ('qVbar', Tree(1)),
+    (('qVbar',), 'VP'): ('qXP', Tree.from_list(['VP', 'V', 0])),
+    (('qXP', 'qVbar'), "VP"): ('qXP', Tree.from_list(['VP', 0, ["V'", 'V', 1]])),
     ((), 'D'): ('qD', Tree('')),
     (('qD',), "D'"): ('qDbar', Tree('')),
     (('qDbar',), 'DP'): ('qXP', Tree('D')),
-    ((), 'I'): ('qI', Tree('T')),
-    (('qI',), "I'"): ('qIbar', Tree.from_list(["T'", 0])),
-    (('qIbar',), 'IP'): ('qXP', Tree.from_list(['TP', 0])),
-    (('qI', 'qXP'), "I'"): ('qIbar', Tree.from_list(["T'", 0, 1])),
-    (('qXP', 'qIbar'), "IP"): ('qXP', Tree.from_list(['TP', 0, 1]))}
+    ((), 'I'): ('qI', Tree('')),
+    (('qI',), "I'"): ('qI', Tree('')),
+    (('qI',), 'IP'): ('qXP', Tree('TP')),
+    (('qI', 'qXP'), "I'"): ('qIbar', Tree(1)),
+    (('qIbar',), 'IP'): ('qXP', Tree.from_list(['TP', 'T', 0])),
+    (('qXP', 'qIbar'), "IP"): ('qXP', Tree.from_list(['TP', 0, ["T'", 'T', 1]]))}
 gb_to_min = TreeBDFT(states, alph, finals, trans)
 
-print(gb_to_min)
-print()
-print(gb_to_min.transform(test_trees.gb_np_n))
-print(gb_to_min.transform(test_trees.gb_np_d_n))
-print(gb_to_min.transform(test_trees.gb_simple_trans_clause, debug=True))
+if __name__ == '__main__':
+    print(gb_to_min)
+    print()
+    print(gb_to_min.transform(test_trees.gb_np_n))
+    print(gb_to_min.transform(test_trees.gb_np_d_n))
+    print(gb_to_min.transform(test_trees.gb_simple_trans_clause, debug=True))
