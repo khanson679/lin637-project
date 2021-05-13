e114db9a4065ee24886a6f8be592109d24f9fd70
Kenneth Hanson <khanson679@gmail.com>
2021-05-13 18:07:24 -0400

    Expand test trees.

--- test_trees.py
+++ test_trees.py
@@ -1,5 +1,16 @@
 from tree import Tree
 
+gb_xp_singleton = Tree.from_list(["XP", ["X'", "X"]])
+gb_xp_w_comp = Tree.from_list(["XP", ["X'", "X", ["YP", ["Y'", "Y"]]]])
+gb_xp_w_comp_spec = Tree.from_list(
+    ["XP",
+     ["ZP", ["Z'", "Z"]],
+     ["X'", "X", ["YP", ["Y'", "Y"]]]])
+gb_xp_w_spec_no_comp = Tree.from_list(
+    ["XP",
+     ["ZP", ["Z'", "Z"]],
+     ["X'", "X"]])
+
 gb_np_n = Tree.from_list(["NP", ["N'", "N"]])
 gb_np_d_n = Tree.from_list(
     ["NP",
@@ -10,14 +21,34 @@ gb_simple_trans_clause = Tree.from_list(
         ["NP",
             ["DP", ["D'", "D"]],
             ["N'", "N"]],
-        ["I'",
-            "I",
+        ["I'", "I",
             ["VP",
-                ["V'",
-                    "V",
+                ["V'", "V",
                     ["NP",
                         ["DP", ["D'", "D"]],
                         ["N'", "N"]]]]]])
+gb_pp_comp_cp_comp = Tree.from_list(
+    ["IP",
+        ["NP",
+            ["DP", ["D'", "D"]],
+            ["N'", "N", ["PP", ["P'", "P", ["NP", ["N'", "N"]]]]]],
+        ["I'", "I",
+            ["VP",
+                ["V'", "V",
+                    ["CP",
+                        ["C'", "C",
+                            ["IP",
+                                ["NP", ["N'", "N"]],
+                                ["I'", "I", ["VP", ["V'", "V"]]]]]]]]]])
+
+min_xp_singleton = Tree.from_list(["XP"])
+min_xp_w_comp = Tree.from_list(["XP", "X", "YP"])
+min_xp_w_comp_spec = Tree.from_list(
+    ["XP", "ZP", ["X'", "X", "YP"]])
+min_xp_w_spec_no_comp = Tree.from_list(
+    ["?P",
+     ["ZP", ["Z'", "Z"]],
+     ["?'", "?", "XP"]])
 
 min_dp_leaf = Tree.from_list(["DP"])
 min_dp_d_n = Tree.from_list(["DP", "D", "NP"])
@@ -29,3 +60,16 @@ min_simple_trans_clause = Tree.from_list(
             ["VP",
                 ["V"],
                 ["DP", "D", "NP"]]]])
+gb_pp_comp_cp_comp = Tree.from_list(
+    ["IP",
+        ["NP",
+            ["DP", ["D'", "D"]],
+            ["N'", "N", ["PP", ["P'", "P", ["NP", ["N'", "N"]]]]]],
+        ["I'", "I",
+            ["VP",
+                ["V'", "V",
+                    ["CP",
+                        ["C'", "C",
+                            ["IP",
+                                ["NP", ["N'", "N"]],
+                                ["I'", "I", ["VP", ["V'", "V"]]]]]]]]]])
